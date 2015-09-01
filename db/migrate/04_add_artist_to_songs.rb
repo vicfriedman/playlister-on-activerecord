b1ba5e5dcfc06b126a0c062bbd75d494ee2e0ea2
vicfriedman <victoria.m.friedman@gmail.com>
2015-09-01 08:44:39 -0400

    done

--- db/migrate/04_add_artist_to_songs.rb
+++ db/migrate/04_add_artist_to_songs.rb
@@ -1,2 +1,6 @@
 class AddArtistToSongs < ActiveRecord::Migration
+
+  def change
+    add_column :songs, :artist_id, :integer
+  end
 end
---
+++ db/migrate/05_add_genre_to_songs.rb
@@ -0,0 +1,6 @@
+class AddGenreToSongs < ActiveRecord::Migration
+
+  def change
+    add_column :songs, :genre_id, :integer
+  end
+end
